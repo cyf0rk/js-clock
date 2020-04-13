bcb70eba57f27ce88c232d455862653505bfb24a
marijan111 <marijanbao@gmail.com>
2020-04-13 15:05:32 +0200

    tik-tok

--- main.js
+++ main.js
@@ -1,27 +1,42 @@
-// function cloneTicks() {
-//   for (let i = 0; i <= 12; i++) {
-//     let fiveMinutes = document.querySelector('.fiveminutes');
-//     fiveMinutes.cloneNode(true);
-//   }
-// }
+/*cloneTicks function positions clock ticks in the right timeframe, 
+minuteTicks: smaller spread ticks and 
+fiveMinuteTicks: bigger spread ticks*/
+function cloneTicks() {
+  for (let i = 0; i <= 60; i++) {
+    let minuteTicks = document.querySelector('.minclock');
+    let clone = minuteTicks.cloneNode(true);
+    document.querySelector('#clockframe').appendChild(clone);
+    clone.style.transform = `rotate(${i * 6}deg)`;
+  }
 
-// cloneTicks();
-
-let hour = document.querySelector('.hour');
-let minute = document.querySelector('.min');
-let second = document.querySelector('.sec');
-
-function setTime() {
-  let date = new Date();
-
-  let dateHour = date.getHours();
-  hour.style.transform = `rotate(${dateHour * 30}deg)`;
-
-  let dateMinute = date.getMinutes();
-  minute.style.transform = `rotate(${dateMinute * 6}deg)`;
-
-  let dateSecond = date.getSeconds();
-  second.style.transform = `rotate(${dateSecond * 6}deg)`;
+  for (let i = 0; i <= 12; i++) {
+    let fiveMinuteTicks = document.querySelector('.fiveminclock');
+    let clone = fiveMinuteTicks.cloneNode(true);
+    document.querySelector('#clockframe').appendChild(clone);
+    clone.style.transform = `rotate(${i * 30}deg)`;
+  }
 }
 
+// catching clock hands as DOM objects in variables for easier use in our code
+let hourHand = document.querySelector('.hour');
+let minuteHand = document.querySelector('.min');
+let secondHand = document.querySelector('.sec');
+
+// this function will give animation to our clock hands depending on the current time set by the Date object
+function setTime() {
+  let time = new Date();
+
+  let hours = time.getHours();
+  hourHand.style.transform = `rotate(${hours * 30}deg)`;
+
+  let minutes = time.getMinutes();
+  minuteHand.style.transform = `rotate(${minutes * 6}deg)`;
+
+  let seconds = time.getSeconds();
+  secondHand.style.transform = `rotate(${seconds * 6}deg)`;
+  secondHand.style.transition = 'transform 0.5s';
+}
+
+cloneTicks();
+// setInterval will call setTime function every second
 setInterval(setTime, 1000);
